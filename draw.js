c3799ce7f50aaaa96f56e6043d9c6838eb1e4e91
davidkorff <davidkorff@gmail.com>
2017-02-07 15:11:04 -0500

    web socket

--- draw.js
+++ draw.js
@@ -41,23 +41,23 @@ window.onload = function() {
 
 
   // Send a message when the form is submitted.
-  form.onsubmit = function(e) {
-    e.preventDefault();
-
-    // Retrieve the message from the textarea.
-    var message = messageField.value;
-
-    // Send the message through the WebSocket.
-    socket.send(message);
-
-    // Add the message to the messages list.
-    messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
-                              '</li>';
-
-    // Clear out the message field.
-    messageField.value = '';
-
-    return false;
+  // form.onsubmit = function(e) {
+  //   e.preventDefault();
+  //
+  //   // Retrieve the message from the textarea.
+  //   var message = messageField.value;
+  //
+  //   // Send the message through the WebSocket.
+  //   socket.send(message);
+  //
+  //   // Add the message to the messages list.
+  //   messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
+  //                             '</li>';
+  //
+  //   // Clear out the message field.
+  //   messageField.value = '';
+  //
+  //   return false;
   };
 
 
